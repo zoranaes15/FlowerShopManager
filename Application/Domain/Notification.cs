beaf4cad2cfff0ae0034b1cf86f93552514a4e96
zoranaes15 <zoranalicina04@gmail.com>
2026-05-24 01:24:56 +0200

    added class Notification

---
+++ Application/Domain/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public class Notification : Entity
+    {
+        public int UserId { get; private set; }
+        public string Message { get; private set; }
+        public bool IsItRead { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
