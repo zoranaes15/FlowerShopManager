c62639bcf2d4a3f7d01ce981d0705798a219fabf
zoranaes15 <zoranalicina04@gmail.com>
2026-05-24 00:36:44 +0200

    Add project files.

---
+++ Application/Domain/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public enum UserRole
+    {
+        Admin,
+        Staff,
+        Customer
+    }
+
+    public class User : Entity
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public UserRole Role { get; private set; }
+
+    }
+}
