1517167918c135abe62868ee38f09d44fdd22f6d
zoranaes15 <zoranalicina04@gmail.com>
2026-05-24 01:20:37 +0200

    added Payment class

---
+++ Application/Domain/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public enum PaymentStatus
+    {
+        Pending,
+        Completed,
+        Failed,
+        Refunded
+    }
+    public class Payment : Entity
+    {
+        public int OrderId { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTime Date { get; private set; }
+        public PaymentStatus Status { get; private set; }
+    }
+}
