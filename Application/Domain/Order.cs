7a4402df9481c60ea8b921262d6b3f1aadd784e6
zoranaes15 <zoranalicina04@gmail.com>
2026-05-24 01:07:36 +0200

    Made order and product classes

---
+++ Application/Domain/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public enum OrderStatus { 
+        pending,
+        processing,
+        completed,
+        cancelled
+    }
+    public class Order : Entity
+    {
+        public int CustomerId { get; private set; }
+        public DateTime OrderDate { get; private set; }
+        public decimal TotalAmount { get; private set; }
+        public OrderStatus Status { get; private set; }
+
+    }
+}
---
+++ Application/Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain
+{
+    public class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+    }
+}
